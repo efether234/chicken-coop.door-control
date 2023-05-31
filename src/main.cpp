cd0c1dc972b430603b1dabad7daec939b6369f61
Evan Fetherolf <efether234@gmail.com>
2023-05-31 13:31:26 -0700

    Connect to MQTT Broker

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,31 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 #include "secrets.h"
 
 WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+
 
 // put function declarations here:
 void connectToWifi();
+void connectToBroker();
 
 void setup() {
   Serial.begin(115200);
   connectToWifi();
+  connectToBroker();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
+/*
+ * Function to Connect to to WiFi
+ */
 void connectToWifi() {
   Serial.println();
   Serial.println();
@@ -34,4 +42,19 @@ void connectToWifi() {
   Serial.println("Connected to the WiFi network.");
   Serial.print("Local IP: ");
   Serial.println(WiFi.localIP());
+}
+
+/*
+ * Connect to MQTT
+ */
+void connectToBroker() {
+  client.setServer("192.168.1.113", 1883);
+  Serial.print("Connecting to MQTT Broker");
+  while (!client.connect("door-controller")) {
+    delay(1000);
+    Serial.print(".");
+  } 
+  Serial.println();
+  Serial.println("Connected to MQTT Broker");
+  client.publish("chateau-sadler/chicken-coop/door-status", "connected!");
 }
