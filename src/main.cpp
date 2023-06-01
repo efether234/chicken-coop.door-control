e8675d28da513ed9306ea9e736764c63bfac59f5
Evan Fetherolf <efether234@gmail.com>
2023-06-01 10:08:47 -0700

    Subscribe to command JSON messages.

--- src/main.cpp
+++ src/main.cpp
@@ -9,35 +9,59 @@ WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
 // Pin Definitions
-const int DOOR_OPEN_SENSOR = 5;
-const int DOOR_CLOSED_SENSOR = 4;
+const int DOOR_OPEN_SENSOR = 12;
+const int DOOR_CLOSED_SENSOR = 13;
+const int MOTOR_CW = 5;
+const int MOTOR_CCW = 4;
 
-// Const Definitions
+/* *****************************************
+ * Topic Definitions
+ *  
+ * *************************************** */
+
+// Publish door open/closed status
 const char* PUB_TOPIC = "chateau-sadler/chicken-coop/door-status";
+
+// Publish MQTT connection status
+const char* CON_TOPIC = "chateau-sadler/chicken-coop/connected";
+
+// Subscribe door control
+char* SUB_TOPIC = "chateau-sadler/chicken-coop/door-control";
 
 // Variable Declarations
 String doorState;
+bool subscribed = false;
 
 // Function Declarations
 void connectToWifi();
 void connectToBroker();
+void callback(char*, byte*, unsigned int);
+void openDoor();
+void closeDoor();
 
 // JSON setup
-const int size = JSON_OBJECT_SIZE(3);
-StaticJsonDocument<size> doc;
+const int capacity = JSON_OBJECT_SIZE(3);
+StaticJsonDocument<capacity> status;
+StaticJsonDocument<capacity> command;
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(DOOR_OPEN_SENSOR, INPUT_PULLUP);
   pinMode(DOOR_CLOSED_SENSOR, INPUT_PULLUP);
+  pinMode(MOTOR_CW, OUTPUT);
+  pinMode(MOTOR_CCW, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
   connectToWifi();
   connectToBroker();
+  client.subscribe(SUB_TOPIC);
+
+  // Serial.println(subscribed);
 }
 
-void loop() {;
+void loop() {
+  client.loop();
   // Read which door sensor is currently active
   // and set the doorState var to 'open', 'closed', or
   // 'other'
@@ -55,13 +79,20 @@ void loop() {;
     digitalWrite(LED_BUILTIN, HIGH);
   }
 
-  doc["door-state"] = doorState.c_str();
+  status["doorState"] = doorState.c_str();
 
   char buffer[256];
-  serializeJson(doc, buffer);
+  serializeJson(status, buffer);
   client.publish(PUB_TOPIC, buffer);
-  delay(2500);
+  delay(100);
+
+  // openDoor();
+  // delay(1000);
+  // closeDoor();
+  // delay(1000);
 }
+
+
 
 /* *****************************************
  *
@@ -69,6 +100,18 @@ void loop() {;
  * 
  * 
  * *************************************** */
+
+// Door controls
+
+void openDoor() {
+  digitalWrite(MOTOR_CW, HIGH);
+  digitalWrite(MOTOR_CCW, LOW);
+}
+
+void closeDoor() {
+  digitalWrite(MOTOR_CCW, HIGH);
+  digitalWrite(MOTOR_CW, LOW);
+}
 
 // Connect to Wifi
 
@@ -94,6 +137,7 @@ void connectToWifi() {
 
 void connectToBroker() {
   client.setServer("192.168.1.113", 1883);
+  client.setCallback(callback);
   Serial.print("Connecting to MQTT Broker");
   while (!client.connect("door-controller")) {
     delay(1000);
@@ -101,5 +145,17 @@ void connectToBroker() {
   } 
   Serial.println();
   Serial.println("Connected to MQTT Broker");
-  client.publish(PUB_TOPIC, "connected!");
+  client.publish(CON_TOPIC, "connected!");
+}
+
+// Callback
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  deserializeJson(command, payload);
+
+  if (command["cmd"] == "open") {
+    openDoor();
+  } else if (command["cmd"] == "close") {
+    closeDoor();
+  }
 }
