ae8f29a6d1ec953eaf5d90a4f7b631dece6e1bd5
Evan Fetherolf <efether234@gmail.com>
2023-06-23 12:36:45 -0700

    Get ElegantOTA functioning.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
 
 #include <secrets.h>
+
+// Pin Definitions
+int motorOpenPin = 5;
+int motorClosePin = 0;
 
 // WiFi Config
 IPAddress local_IP(192, 168, 1, 109);
 IPAddress gateway(192, 168, 1, 254);
 IPAddress subnet(255, 255, 255, 0);
 
+WiFiClient wifiClient;
+
+AsyncWebServer server(80);
+
 void setup() {
+    // Setup pins
+    pinMode(motorOpenPin, OUTPUT);
+    digitalWrite(motorOpenPin, LOW);
+    pinMode(motorClosePin, OUTPUT);
+    digitalWrite(motorClosePin, LOW);
+
     Serial.begin(115200);
     Serial.println();
 
@@ -32,8 +49,14 @@ void setup() {
     Serial.println("Connected to WiFi network.");
     Serial.print("Local IP: ");
     Serial.println(WiFi.localIP());
+
+    AsyncElegantOTA.begin(&server);
+    server.begin();
 }
 
 void loop() {
-
+    digitalWrite(motorOpenPin, HIGH);
+    delay(100);
+    digitalWrite(motorOpenPin, LOW);
+    delay(900);
 }
