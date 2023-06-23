38d98b5564809482e52b1e3552c2295f4c6a86d9
Evan Fetherolf <efether234@gmail.com>
2023-06-23 12:19:57 -0700

    Add connect to WiFi code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,39 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <AsyncElegantOTA.h>
 
 #include <secrets.h>
 
-IPAddress local_IP(192, 168, 1, 108);
+// WiFi Config
+IPAddress local_IP(192, 168, 1, 109);
 IPAddress gateway(192, 168, 1, 254);
 IPAddress subnet(255, 255, 255, 0);
 
-AsyncWebServer server(80);
-
-void setup()
-{
+void setup() {
     Serial.begin(115200);
-    WiFi.mode(WIFI_STA);
     Serial.println();
+
+    if (!WiFi.config(local_IP, gateway, subnet))
+    {
+        Serial.println("WiFi failed to configure");
+    }
+
     Serial.print("Connecting to WiFi network: ");
     Serial.print(SECRET_SSID);
+
     WiFi.begin(SECRET_SSID, SECRET_PASS);
-    
-    while(WiFi.status() != WL_CONNECTED)
+
+    while (WiFi.status() != WL_CONNECTED)
     {
         delay(1000);
         Serial.print(".");
     }
+
     Serial.println();
-    Serial.println("Connected to the WiFi network.");
+    Serial.println("Connected to WiFi network.");
     Serial.print("Local IP: ");
     Serial.println(WiFi.localIP());
-    
 }
 
-void loop()
-{
+void loop() {
 
 }
